bf0712803bf7c5a3b0bb7d263ac553459a3e6810
GKurkhuli <giorgi.kurkhuli1@gmail.com>
2023-12-10 16:37:53 +0400

    ADD: add index.js file

---
+++ coffee-house/src/script/index.js
@@ -0,0 +1,5 @@
+console.log("1.Checking validation of pages: +16");
+console.log("2.The layout matches the design +42");
+console.log("CSS Requirements +8: when scaling down browser page it shifts to the side");
+console.log("Interactivity +32");
+console.log("Final Score: 98");
